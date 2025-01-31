cc5aa6929c3e2287325f6cf45f9b1cdb2da163f8
Rukotana Freddy <rukotanafreddy@gmail.com>
2025-01-31 18:37:29 +0200

    Final commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
- 
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb://mongo:emYRPEKukPSVgxMHyQgQEkkjQsyaPBDS@viaduct.proxy.rlwy.net:54352');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
