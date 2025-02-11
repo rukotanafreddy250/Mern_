030b25516b3af128486415b4deac3be470e0cbb2
Rukotana Freddy <39240583+rukotanafreddy250@users.noreply.github.com>
2025-02-11 13:56:40 +0000

    ready for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://mongo:emYRPEKukPSVgxMHyQgQEkkjQsyaPBDS@viaduct.proxy.rlwy.net:54352');
+    // const conn = await mongoose.connect('mongodb://mongo:emYRPEKukPSVgxMHyQgQEkkjQsyaPBDS@viaduct.proxy.rlwy.net:54352');
+    const conn = await mongoose.connect(mongodb://mongo:nwtztBptINcSfSHXhjCyyUBajnduvalZ@roundhouse.proxy.rlwy.net:36824');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
