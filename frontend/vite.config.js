84a263b6e7eaf6972148c9c24a3cc3f3adfb915c
Rukotana Freddy <39240583+rukotanafreddy250@users.noreply.github.com>
2025-02-12 09:32:41 +0000

    Final commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config(); // Load .env variables into process.env
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000,
     proxy: {
       '/api': {
-        target: 'https://mern-2-gohh.onrender.com',
+        target: process.env.VITE_REQ_URL,
         changeOrigin: true,
       },
     },
